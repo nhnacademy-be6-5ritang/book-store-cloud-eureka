1b07a8ce3a9a36fca30d05c2954c4740aa85bc72
kihoo-ni <gudcjf532@gmail.com>
2024-06-18 10:56:25 +0900

    Feat: "유레카 기능 구현"
    유레카 기능 구현
    
    Related to: #110

---
+++ src/main/java/com/nhnacademy/bookstoreeureka/BookStoreEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.bookstoreeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class BookStoreEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreEurekaApplication.class, args);
+    }
+
+}
